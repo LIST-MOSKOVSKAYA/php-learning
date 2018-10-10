3051c6bdec552b0e414cd817e19afe42841c8bc9
Molga <moskovskayaolga@gmail.com>
2018-10-10 14:25:23 +0300

    Реализованы замечания

--- php/me/test1_add.php
+++ php/me/test1_add.php
@@ -1,22 +1,31 @@
-<?php
-   $x = rand(0,100);
-   echo "<p>Число ", $x, "</p>";
-   $number_1 = 1;
-   $number_2 = 1;
-   for ($i = 0; $i<=100; $i++)
-   {
-   if ($number_1 > $x) {
-     echo "Число ", $x," не входит в числовой ряд";
-     break;
-   } else {
-   if ($number_1 == $x) {
-     echo "Число ", $x," входит в числовой ряд";
-     break;
-   } else {
-   $number_3 = $number_1;
-   $number_1 += $number_2;
-   $number_2 = $number_3;
-   }
-   }
-   }
-?>
+<!DOCTYPE html>
+<html lang='ru'>
+   <head>
+      <meta charset='UTF-8'>
+      <title>Домашнее задание #1, дополнение</title>
+   </head>
+   <body>
+      <?php
+         $x = rand(0,100);
+         $number_1 = 1;
+         $number_2 = 1;
+
+         echo '<p>Число ', $x, '</p>';
+         for ($i = 0; $i <= 100; $i++) {
+         if ($number_1 < $x) {
+           $number_3 = $number_1;
+           $number_1 += $number_2;
+           $number_2 = $number_3;
+         } else {
+         break;
+         }
+         }
+         if ($number_1 > $x) {
+         echo 'Число ', $x,' не входит в числовой ряд';
+         }
+         else if ($number_1 == $x) {
+         echo 'Число ', $x,' входит в числовой ряд';
+         }
+         ?>
+   </body>
+</html>
